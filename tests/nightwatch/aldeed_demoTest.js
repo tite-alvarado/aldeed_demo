d3190020a04a47e85a624972734d6b93aec25670
felipe <felipe@localhost>
2016-11-21 01:25:21 -0600

    AL-7 : Adding nitghwatchjs tests

---
+++ tests/nightwatch/aldeed_demoTest.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'Aldeed Demo - Create/Delete User' : function (client) {
+    client
+      .url('http://192.168.122.16:3000')
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('.container', 'Registered People')
+      .setValue('input[name=firstName]', 'Happy QA')
+      .setValue('input[name=lastName]', 'Users Last Name')
+      .setValue('input[name=password]', '1234')
+      .setValue('input[name=age]', '10')
+      .setValue('input[name=birthDate]', '1925-10-25')
+      .click('select[name=gender] option[value=Male]')
+      .setValue('input[name=email]', 'test@dev.test')
+      .click('.btn-primary')
+      .pause(1000)
+      .assert.containsText('.table', 'Happy QA')
+      .pause(1000)
+      .click('.table tbody tr td a')
+      .click('.btn-danger')
+      .pause(3000)
+      .acceptAlert()
+      .pause(1000)
+      .assert.elementNotPresent('.table tbody tr td a')
+      .end();
+  }
+};
